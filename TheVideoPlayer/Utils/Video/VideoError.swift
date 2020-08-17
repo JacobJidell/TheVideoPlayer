4eee3fdc55ed1534c52def6d1af45271c6f2d148
Jacob Ahlberg <jacob.ahlberg@knowit.se>
2020-08-17 08:42:09 +0200

    Added local json file with different kind of videos

---
+++ TheVideoPlayer/Utils/Video/VideoError.swift
@@ -0,0 +1,24 @@
+//
+//  VideoError.swift
+//  TheVideoPlayer
+//
+//  Created by Jacob Ahlberg on 2020-08-17.
+//  Copyright © 2020 Knowit Mobile. All rights reserved.
+//
+
+import Foundation
+
+enum VideoError: LocalizedError {
+    case videoDoesNotExist
+    case invalidPath
+    case failedToDecode(Error)
+    case undetermined(Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .invalidPath: return "The path is invalid."
+        case .videoDoesNotExist: return "The video does not exist."
+        case .undetermined(let error): return "Failed with error: \(error)"
+        }
+    }
+}
